fb5a6db4380f3a4e9a334c7b2c567220ad42484a
Eric Mountain <eric.mountain@gmail.com>
2025-03-02 15:41:55 +0100

    Add voyager

---
+++ qmk_userspace/moonlander2voyager.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define LAYOUT_moonlander(k0A, k0B, k0C, k0D, k0E, k0F, k0G, k6A, k6B, k6C, k6D, k6E, k6F, k6G, k1A, k1B, k1C, k1D, k1E, k1F, k1G, k7A, k7B, k7C, k7D, k7E, k7F, k7G, k2A, k2B, k2C, k2D, k2E, k2F, k2G, k8A, k8B, k8C, k8D, k8E, k8F, k8G, k3A, k3B, k3C, k3D, k3E, k3F, k9B, k9C, k9D, k9E, k9F, k9G, k4A, k4B, k4C, k4D, k4E, k5D, kBD, kAC, kAD, kAE, kAF, kAG, k5A, k5B, k5C, kBE, kBF, kBG)  { \
+    {KC_NO, k0A, k0B, k0C, k0D, k0E, k0F}, \
+    {KC_NO, k1A, k1B, k1C, k1D, k1E, k1F}, \
+    {KC_NO, k2A, k2B, k2C, k2D, k2E, k2F}, \
+    {KC_NO, k3A, k3B, k3C, k3D, k3E, KC_NO}, \
+    {KC_NO, KC_NO, KC_NO, KC_NO, k3F, KC_NO, KC_NO}, \
+    {k5A, k5B, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO}, \
+    {k6B, k6C, k6D, k6E, k6F, k6G, KC_NO}, \
+    {k7B, k7C, k7D, k7E, k7F, k7G, KC_NO}, \
+    {k8B, k8C, k8D, k8E, k8F, k8G, KC_NO}, \
+    {KC_NO, k9C, k9D, k9E, k9F, k9G, KC_NO}, \
+    {KC_NO, KC_NO, k9B, KC_NO, KC_NO, KC_NO, KC_NO}, \
+    {KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, kBF, kBG} \
+}
