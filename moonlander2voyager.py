e52a3fa0412b1047a37e045b0907c9f10574275e
Eric Mountain <eric.mountain@gmail.com>
2025-03-09 16:18:56 +0100

    Cleanup and comment the conversion script
    Mostly so I understand what it does 6 months from now.

--- moonlander2voyager.py
+++ moonlander2voyager.py
@@ -3,13 +3,20 @@
 import re
 import fileinput
 
-# Key is the index of the colour in the ledmap. Value is the index into the
-# moonlander ledmap.
+# Array offset is the index of the colour in the ledmap. Value is the offset into the
+# Moonlander ledmap.
+#
+# Note the Moonlander ledmap goes column by column, whereas the Voyager’s is row by row,
+# so we’re doing a matrix transposition of sorts.
+#
+# Left block
 # 0, 5, 10, 15, 20, 25,
 # 1, 6, 11, 16, 21, 26,
 # 2, 7, 12, 17, 22, 27,
 # 3, 8, 13, 18, 23, 28,
 # 32, 33,
+#
+# Right block
 # 61, 56, 51, 46, 41, 36,
 # 62, 57, 52, 47, 42, 37,
 # 63, 58, 53, 48, 43, 38,
@@ -78,17 +85,12 @@ def handle_array_entry(line):
 
     rx = re.compile(r"(\s*\[\d+\] = \{\s*)((\{\d+,\d+,\d+\},*\s*)+)\},")
     match = rx.findall(line)
-    # print(f"Matches: {match}")
 
     index_prefix = match[0][0]
-    # print(f"Index: {index_prefix}")
-
     triplets_string = match[0][1]
-    # print(f"triplets: {triplets_string}")
 
     triplets_rx = re.compile(r"\{(\d+),(\d+),(\d+)\}")
     triplets = triplets_rx.findall(triplets_string)
-    # print(f"triplets: {triplets}")
 
     print(index_prefix, end="")
     for i in range(len(voyager2moonlander)):
@@ -110,20 +112,21 @@ parsing_array = False
 for line in fileinput.input():
     array_in_line = line.find(array_signature)
     if not parsing_array and array_in_line != -1:
+        # Start of ledmap array found
         parsing_array = True
         print(line, end="")
         continue
 
+    # Each line of the array declaration is the ledmap for one layer
     if parsing_array:
-        # check if end of array
         end_of_array = line.find(array_end_signature)
         if end_of_array != -1:
+            # End of ledmap array found, resume normal processing
             parsing_array = False
             print(line, end="")
             continue
-        # print(f"todecode: {line}")
         handle_array_entry(line)
-        # sys.exit(1)
         continue
 
+    # Any line that isn’t part of the ledmap array we simply output
     print(line, end="")
